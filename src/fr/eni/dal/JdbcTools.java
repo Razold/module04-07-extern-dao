4277683a2e63518feb360b15673fd8ad029f2916
dhannane2021 <boryu.jul@gmail.com>
2021-07-08 14:49:25 +0200

    1er commit

---
+++ src/fr/eni/dal/JdbcTools.java
@@ -0,0 +1,32 @@
+package fr.eni.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcTools {
+
+	private static String url;
+	private static String driver;
+	private static String login;
+	private static String passw;
+	
+	
+	static {
+		
+		url = Settings.getProperty("urldb");
+		driver = Settings.getProperty("driverdb");
+		login = Settings.getProperty("userdb");
+		passw = Settings.getProperty("passdb");
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Attention le driver n'a pas été trouvé");
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, login, passw);
+	}
+	
+}
